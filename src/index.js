1384ffe35ea3ad550c04f162a9932dca106eb0ae
Uldarico Estrada <uldarico@outlook.com>
2018-10-23 12:37:20 -0700

    Setup Redux-Store
    -Setup the initial state/redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Counter from './Counter';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+function reducer() {
+  // returns undefined
+  return {
+    count: 42
+  };
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
